7e8d370526be08a9cdb9ea78e758baabae052e3d
Stepanov <vutt.gray@gmail.com>
2021-09-02 22:21:45 +0300

    Add transfer between pages

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,5 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 
 MAIN_LINK = 'http://selenium1py.pythonanywhere.com/'
@@ -8,6 +9,10 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, MAIN_LINK)
     page.open()
     page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_link)
+    login_page.should_be_login_url()
+    login_page.should_be_login_form()
+    login_page.should_be_register_form()
 
 
 def test_guest_should_see_login_link(browser):
