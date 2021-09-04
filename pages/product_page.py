781b606ac81301cf767622124c302c8a214c2c82
Stepanov <vutt.gray@gmail.com>
2021-09-04 17:22:45 +0300

    Test success message after adding product to basket

--- pages/product_page.py
+++ pages/product_page.py
@@ -13,11 +13,24 @@ class ProductPage(BasePage):
         self.click_element(*ProductPageLocators.ADD_PRODUCT_BUTTON)
 
     def should_be_product_added_to_basket(self, title: str):
-        assert self.is_element_present(*ProductPageLocators.ADDED_PRODUCT_TITLE)
+        self.should_be_success_message_after_adding_product_to_basket()
         added_product_message = self.get_element_text(*ProductPageLocators.ADDED_PRODUCT_TITLE)
         assert title == added_product_message, 'Basket message does not contain the product title'
 
     def should_be_basket_price(self, price):
-        assert self.is_element_present(*ProductPageLocators.BASKET_PRICE)
+        assert self.is_element_present(*ProductPageLocators.BASKET_PRICE), \
+            'Basket total price is not presented, but should be'
         basket_price = self.get_element_text(*ProductPageLocators.BASKET_PRICE)
         assert price == basket_price, 'Basket total price is not equal to the added product price'
+
+    def should_be_success_message_after_adding_product_to_basket(self):
+        assert self.is_element_present(*ProductPageLocators.ADDED_PRODUCT_TITLE), \
+            'Success message is not presented, but should be'
+
+    def should_not_be_success_message_after_adding_product_to_basket(self):
+        assert self.is_not_element_present(*ProductPageLocators.ADDED_PRODUCT_TITLE), \
+            'Success message is presented, but should not be'
+
+    def should_be_disappeared_success_message_after_adding_product_to_basket(self):
+        assert self.is_disappeared(*ProductPageLocators.ADDED_PRODUCT_TITLE), \
+            'Success message is still presented, but should be disappeared'
--- test_product_page.py
+++ test_product_page.py
@@ -6,6 +6,7 @@ PRODUCT_LINK = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_2
 # 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
 
 
+@pytest.mark.skip
 @pytest.mark.parametrize('link', [PRODUCT_LINK + "?promo=offer0",
                                   PRODUCT_LINK + "?promo=offer1",
                                   PRODUCT_LINK + "?promo=offer2",
@@ -26,3 +27,25 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.solve_quiz_and_get_code()
     page.should_be_product_added_to_basket(product_title)
     page.should_be_basket_price(product_price)
+
+
+@pytest.mark.skip
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, PRODUCT_LINK, timeout=0)
+    page.open()
+    page.add_product()
+    page.should_not_be_success_message_after_adding_product_to_basket()
+
+
+def test_guest_cant_see_success_message(browser):
+    page = ProductPage(browser, PRODUCT_LINK, timeout=0)
+    page.open()
+    page.should_not_be_success_message_after_adding_product_to_basket()
+
+
+@pytest.mark.xfail
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, PRODUCT_LINK, timeout=0)
+    page.open()
+    page.add_product()
+    page.should_be_disappeared_success_message_after_adding_product_to_basket()
