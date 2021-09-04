d017982ea1759e80e59595462a7a0de13e9254ea
Stepanov <vutt.gray@gmail.com>
2021-09-04 16:49:09 +0300

    Test product adding for several promos

--- test_product_page.py
+++ test_product_page.py
@@ -1,11 +1,23 @@
+import pytest
 from .pages.product_page import ProductPage
 
-LINK = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
+PRODUCT_LINK = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+# 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
 # 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
 
 
-def test_guest_can_add_product_to_basket(browser):
-    page = ProductPage(browser, LINK)
+@pytest.mark.parametrize('link', [PRODUCT_LINK + "?promo=offer0",
+                                  PRODUCT_LINK + "?promo=offer1",
+                                  PRODUCT_LINK + "?promo=offer2",
+                                  PRODUCT_LINK + "?promo=offer3",
+                                  PRODUCT_LINK + "?promo=offer4",
+                                  PRODUCT_LINK + "?promo=offer5",
+                                  PRODUCT_LINK + "?promo=offer6",
+                                  pytest.param(PRODUCT_LINK + "?promo=offer7",  marks=pytest.mark.xfail),
+                                  PRODUCT_LINK + "?promo=offer8",
+                                  PRODUCT_LINK + "?promo=offer9"])
+def test_guest_can_add_product_to_basket(browser, link):
+    page = ProductPage(browser, link)
     page.open()
     product_title = page.get_product_title()
     product_price = page.get_product_price()
